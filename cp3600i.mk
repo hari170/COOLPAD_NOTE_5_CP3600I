3967d5c41bda278459bc4b52b2a80818233e5e82
hari patel <34165299+hari170@users.noreply.github.com>
2018-11-19 16:20:39 +0530

    Update cp3600i.mk

--- cp3600i.mk
+++ cp3600i.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from tulip device
-$(call inherit-product, device/yulong/cp3600i/device.mk)
+$(call inherit-product, device/coolpad/cp3600i/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cp3600i
