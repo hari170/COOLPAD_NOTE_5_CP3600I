7fe6d6ac13e158461b31884967b5702c55b131c5
hari patel <34165299+hari170@users.noreply.github.com>
2018-11-24 11:05:00 +0530

    Update aokp.mk

--- aokp.mk
+++ aokp.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/yulong/CP3600i/full_CP3600i.mk)
+$(call inherit-product, device/yulong/CP3600I/full_CP3600I.mk)
 
 # Inherit some common AOKP stuff.
 $(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
@@ -6,10 +6,10 @@ $(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 PRODUCT_NAME := aokp_CP3600I
 BOARD_VENDOR := yulong
 TARGET_VENDOR := yulong
-PRODUCT_DEVICE := CP3600i
+PRODUCT_DEVICE := CP3600I
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="CP3600i" \
-    PRODUCT_NAME="CP3600i" \
-    BUILD_FINGERPRINT="yulong/CP3600i/CP3600i:7.1.1/NMF26V/20170830.223936:user/release-keys" \
-    PRIVATE_BUILD_DESC="CP3600i-user 7.1.1 NMF26V 20170830.223936 release-keys"
+    PRODUCT_DEVICE="CP3600I" \
+    PRODUCT_NAME="CP3600I" \
+    BUILD_FINGERPRINT="Coolpad/CP3600I/CP3600i:7.1.1/NRD90M/20170830.223936:user/release-keys" \
+    PRIVATE_BUILD_DESC="CP3600I-user 7.1.1 NRD90M 20170830.223936 release-keys"
